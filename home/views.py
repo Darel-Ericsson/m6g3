e193f5b43a1387e869b070da0588142fa7695875
Cristobal <cr.inzza@gmail.com>
2023-07-17 21:25:45 -0400

    first commit

---
+++ home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
+import locale
+
+# Create your views here.
+
+
+def bienvenida(request):
+    locale.setlocale(locale.LC_TIME, 'es_ES')
+    time = datetime.datetime.now().strftime('%A %d/%m/%Y %H:%M:%S').capitalize()
+    return render(request, "home/index.html", {
+        'time' : time
+    })
